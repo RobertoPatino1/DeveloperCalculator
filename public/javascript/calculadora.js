363b833ed2a44434a5c34c599fb6a1644a504a01
RobertoPatino1 <rapatino@espol.edu.ec>
2023-06-14 11:55:03 -0500

    Added another field

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -3,11 +3,12 @@ let funcionPredeterminada = () =>{
     formulario.addEventListener('submit', (evento) => {
         evento.preventDefault();    //Para detener el comportamiento predeterminado
         let valorPorHora = parseFloat(document.getElementById('valorPorHora').value)
-        let testingHoras = parseFloat(document.getElementById('testingHoras').value)
         let interfazHoras = parseFloat(document.getElementById('interfazHoras').value)
+        let estiloHoras = parseFloat(document.getElementById('estiloHoras').value)
+        let testingHoras = parseFloat(document.getElementById('testingHoras').value)
         let mantenimientoHoras = parseFloat(document.getElementById('mantenimientoHoras').value)
         let deploymentHoras = parseFloat(document.getElementById('deploymentHoras').value)
-        let resultado = (valorPorHora * interfazHoras) + (valorPorHora * testingHoras) + (valorPorHora*mantenimientoHoras) + (valorPorHora*deploymentHoras)
+        let resultado = (valorPorHora * interfazHoras)+ (valorPorHora * estiloHoras) + (valorPorHora * testingHoras) + (valorPorHora*mantenimientoHoras) + (valorPorHora*deploymentHoras)
 
         document.getElementById('valorTotal').value = resultado;
 
